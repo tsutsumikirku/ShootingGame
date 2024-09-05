4d79e453ec20b307529f12332dc3f4c5477ef01e
mikenchi <mikenti337564@gmail.com>
2024-09-05 19:36:21 +0900

    クロスヘアの実装

--- Assets/Scripts/ShootingManager.cs
+++ Assets/Scripts/ShootingManager.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 
 public class ShootingManager : MonoBehaviour
 {
-    [SerializeField] Camera mainCamera;
+    [SerializeField] Camera _mainCamera;
     [SerializeField] float _shootInterbal;
     [SerializeField] float _zOffSet = 10;
+    [SerializeField] GameObject _closshair;
     float _shoottime;
+
+    private void Start()
+    {
+    }
+
     void Update()
     {
+        Cursor.visible = false;
+        _shoottime += Time.deltaTime;
         Vector3 mousePosition = Input.mousePosition;
+        _closshair.transform.position = mousePosition;
         mousePosition.z = _zOffSet;
-        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
-        Ray shoot = new Ray(transform.position,worldPosition);
-        if(_shootInterbal < _shoottime && Input.GetMouseButtonDown(0))
+        Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(mousePosition);
+        Vector3 direction = (worldPosition - transform.position).normalized;   
+        if (_shootInterbal < _shoottime && Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(shoot, out RaycastHit hit))
+            if (Physics.Raycast(transform.position, direction, out RaycastHit hit))
             {
-                
             }
             _shoottime = 0;
         }
-
     }
 }
